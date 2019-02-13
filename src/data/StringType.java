bca875ba0ed5dcc9ca79245788a148ec78f0f44d
Administrator <Administrator@USER-PC>
2019-02-13 11:58:25 +0900

    StringType 수정

--- src/data/StringType.java
+++ src/data/StringType.java
@@ -5,5 +5,7 @@ public class StringType {
 	public static void main(String[] args) {
 		String str = "123";
 		System.out.println(str+123+1); // 문자 + 숫자는 = 문자가 된다. 더하기는 더하기연산자와 글자를 더하는 역할
+		String str2 = new String("123");
+		System.out.println(str.contentEquals(str2));
 	}
 }
