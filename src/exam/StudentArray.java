1776a035b636e24bc5bfdae9e3c65c63076c5d85
asd9211 <drrg@naver.com>
2019-02-15 17:41:58 +0900

    2.15 배운거 수정

--- src/exam/StudentArray.java
+++ src/exam/StudentArray.java
@@ -14,8 +14,8 @@ public class StudentArray {
 		Student[] students = new Student[10];
 		for (int i = 0; i < students.length; i++) {
 
+			students[i] = new Student(); // 0~9번방까지 new 생성자호출을 해서 Null의 값을 0으로 바꿔주는 것.
 
-			students[i] = new Student();
 			students[i].grade = i+1;
 			Student ce = new Student();
 		}
